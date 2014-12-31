fe4fdc756430dbb998eda9b4ec216b5013c4ca20
Mike Trupkin <mtrupkin@s4isystems.com>
2014-12-31 13:24:24 -0800

    re-add size implicit

--- src/main/scala/core/Size.scala
+++ src/main/scala/core/Size.scala
@@ -12,7 +12,11 @@ case class Size(width: Int, height: Int) {
   def foreach(f: (Int, Int) => Unit): Unit = for ( x <- 0 until width; y <- 0 until height ) f(x, y)
 }
 
-
+object Size {
+  implicit def DoubleTupleToSize(t: (Double, Double)): Size = Size(t._1.toInt, t._2.toInt)
+  implicit def TupleToSize(t: (Int, Int)): Size = Size(t._1, t._2)
+  implicit def SizeToTuple(s: Size): (Int, Int) = (s.width, s.height)
+}
 
 object Sizes {
   val ZERO = new Size(0, 0)
