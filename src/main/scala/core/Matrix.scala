2203e86dad867cc221e3cd175026f1a24de71126
Mike Trupkin <mtrupkin@s4isystems.com>
2015-01-07 13:12:14 -0800

    add mutable matrix using core classes

---
+++ src/main/scala/core/Matrix.scala
@@ -0,0 +1,15 @@
+package me.mtrupkin.core
+
+import scala.reflect.ClassTag
+import scala.Array._
+
+/**
+ * Created by mtrupkin on 1/7/2015.
+ */
+
+// mutable matrix
+class Matrix[T: ClassTag](val size: Size) {
+  val elems = ofDim[T](size.width, size.height)
+  def apply(p: Point): T = elems(p.x)(p.y)
+  def update(p: Point, value: T): Unit = elems(p.x)(p.y) = value
+}
