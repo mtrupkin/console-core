89935601491bb28234db7ab83b7175161b26a717
Mike Trupkin <mike.trupkin@gmail.com>
2015-02-01 21:31:21 -0800

    add foreach method

--- src/main/scala/core/Matrix.scala
+++ src/main/scala/core/Matrix.scala
@@ -12,4 +12,5 @@ class Matrix[T: ClassTag](val size: Size) {
   val elems = ofDim[T](size.width, size.height)
   def apply(p: Point): T = elems(p.x)(p.y)
   def update(p: Point, value: T): Unit = elems(p.x)(p.y) = value
+  def foreach(f: (Point, T) => Unit) = size.foreach(p => f(p, this(p)))
 }
